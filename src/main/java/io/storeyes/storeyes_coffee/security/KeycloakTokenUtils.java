dcea1466816f8ee26f334afb816c355848859ab2
benaarrouch <benaarrouch.mohamed01@gmail.com>
2025-12-27 01:16:07 +0100

    update java version

--- src/main/java/io/storeyes/storeyes_coffee/security/KeycloakTokenUtils.java
+++ src/main/java/io/storeyes/storeyes_coffee/security/KeycloakTokenUtils.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.util.List;
 import java.util.Map;
 
 /**
